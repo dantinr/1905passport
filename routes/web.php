5385a8418753c5c70d0a8f57c5f5c9694a27e95b
liwei <liwei@localhost.localdomain>
2020-01-10 15:13:19 +0800

    登录 注册

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/api/user/reg','User\UserController@reg');     //注册
+Route::post('/api/user/login','User\UserController@login'); // 登录
+Route::get('/api/user/info','User\UserController@userInfo'); // 获取用户信息
